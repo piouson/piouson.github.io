675c155946ff950421d399426093c6b924823329
piouson <piouson@gmail.com>
2020-08-28 07:12:00 +0100

    refactor: update styling to scss
    - drop stylelint
    - update CI workflow
    - add prettier
    - refactor scss styling
    
    Resolve #9

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import "bootstrap";
 import "./scss/style.scss";
 import "jquery";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/brands";
+import "@fortawesome/fontawesome-free/js/solid";
