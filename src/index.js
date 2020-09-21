6a26a93be28f7d49cb11dc96e9c5f81cabe10cd9
piouson <piouson@gmail.com>
2020-09-21 21:41:46 +0100

    fix: refactor to semantic html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import "bootstrap";
 import "./scss/style.scss";
-import "jquery";
 import "@fortawesome/fontawesome-free/js/fontawesome";
 import "@fortawesome/fontawesome-free/js/brands";
 import "@fortawesome/fontawesome-free/js/solid";
